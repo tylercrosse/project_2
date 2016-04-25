87b6e5167786f22c6615e4197ef732655a3eaa58
Lauren C. Kennedy <laurenckennedy@gmail.com>
2016-04-25 17:56:49 -0400

    adds crud features for items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,4 +9,41 @@ def show
   @item = Item.find(params[:id])
 end
 
+def new
+  @item = Item.new()
+end
+
+def create
+  @item = Item.create(item_params)
+  if @item.save
+    redirect_to @item, notice: "Item Successfully Created"
+  else
+    render 'new'
+  end
+end
+
+def edit
+  @items = Item.all
+  @item = Item.find(params[:id])
+end
+
+def update
+  @item = Item.find(params[:id])
+  if @item.update(item_params)
+    redirect_to @item, notice: "Item Successfully Updated"
+  else
+    render 'edit'
+  end
+end
+
+def destroy
+  @item = Item.find(params[:id])
+  @item.destroy
+  redirect_to items_path, notice: "Item Successfully Discarded"
+end
+
+private
+def item_params
+  params.require(:item).permit(:description, :photo_url, :value, :brand, :size, :fabric, :garment_care)
+end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,4 @@
 class Item < ActiveRecord::Base
+  validates :description, :photo_url, presence:true
+
 end
