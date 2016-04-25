95e1244ef4af572a38e8fde7c97e9624ec899ca2
Lauren C. Kennedy <laurenckennedy@gmail.com>
2016-04-25 15:18:29 -0400

    adds items controller, index view of items, styling

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Item.destroy_all
+
+Item.create([
+  {description:"Convertible Pantsuit", photo_url:"http://cdn.shopify.com/s/files/1/0207/1406/products/DSC_0722-2.jpg?v=1461260821", value:"98.00", brand:"Seamly", size:"S/M", fabric:"Rayon/Lycra", garment_care:"Machine wash"},
+  {description:"The Everlane Anorak", photo_url:"https://everlane.imgix.net/i/f1b84318_61b6.jpg?dpr=1&q=65?w=442&h=442&q=65&dpr=1", value:"98.00", brand:"Everlane", size:"S", fabric:"100% water-resistant polyester", garment_care:"Machine wash cold, tumble dry low"},
+  {description: "Linen Jogger Pant", photo_url:"http://athleta.gap.com/webcontent/0009/594/940/cn9594940.jpg", value:"79.00", brand:"Athleta", size: "4", fabric:"Linen", garment_care:"Machine wash"},
+  {description:"DOE Parka", photo_url:"http://cdn.shopify.com/s/files/1/1038/9388/products/Doe_Female_Parka_large.png?v=1459463423", value:"675.00", brand:"Mammoth Outerwear", size:"S", fabric:"PolyCotton", garment_care:"Dry clean"},
+  {description:"White Circle Shirt", photo_url:"http://static1.squarespace.com/static/56338447e4b0bd1d35bf1aa3/56339741e4b0a850f6952461/569e832ed8af102cbcb0cb8a/1453228848834/shirt2edit.jpg?format=300w", brand:"Miakoda", value:"68.00", size:"OS", fabric:"Organic Bamboo & Cotton Blend", garment_care:"Machine wash cold, tumble dry medium, cool iron if needed, do not bleach"},
+
+  ])
